376fa4155cb4f15d561e93ebf1e4dd251e776fae
Joseph Boscan <joeypc.py@gmail.com>
2021-11-12 16:02:08 -0400

    Adding validation on inputs

--- static/js/main.js
+++ static/js/main.js
@@ -17,6 +17,12 @@ addEventListener("keyup", (event) => {
     }
 })
 
+function isNumberKey(evt){
+    var charCode = (evt.which) ? evt.which : evt.keyCode
+    if (charCode > 31 && (charCode < 48 || charCode > 57))
+        return false;
+    return true;
+}
 
 
 
