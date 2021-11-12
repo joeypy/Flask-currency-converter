106f74a5b9a5dc8054822a3003d7e9ad7d3f0ee9
Joseph Boscan <joeypc.py@gmail.com>
2021-11-12 17:01:05 -0400

    updating algoritm for filter number and dots

--- static/js/main.js
+++ static/js/main.js
@@ -19,7 +19,8 @@ addEventListener("keyup", (event) => {
 
 function isNumberKey(evt){
     var charCode = (evt.which) ? evt.which : evt.keyCode
-    if (charCode > 31 && (charCode < 48 || charCode > 57))
+    console.log(charCode)
+    if (charCode > 31 && (charCode < 46 || charCode > 57) || charCode == 47)
         return false;
     return true;
 }
